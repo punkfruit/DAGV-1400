63590dd1bb90de1150a93ba44b7897fb1bca1593
punkfruit <thecybergrape@gmail.com>
2024-09-05 20:05:40 -0600

    Scripting 2: Challenge
    added fields to be able to change the speed from within the inspector

--- Scripting 2/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ Scripting 2/GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class TransformController : MonoBehaviour
 {
     public Transform ChildTransform;
+
+    public float speed = 1;
+    public int pingAmount = 3;
     // Update is called once per frame
     void Update()
     {
         // Move the target Gameobject
-        var x = Mathf.PingPong(Time.time, 3);
+        var x = Mathf.PingPong(Time.time * speed, pingAmount);
         var p = new Vector3(0, x, 0);
         ChildTransform.position = p;
 
